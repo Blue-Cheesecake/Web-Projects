b4b763828d04dd86d29f92c6f53aec0c1a985fd8
SINUT WATTANRPORN <s_w.piepie@outlook.com.au>
2022-03-08 12:10:04 +0700

    Update

--- intro-component-with-signup-form-master/js/components/input.js
+++ intro-component-with-signup-form-master/js/components/input.js
@@ -1,1 +1,35 @@
-const checkValidity = () => {};
+import {
+  remove_margin_bottom_1o25em_class_by_id,
+  add_margin_bottom_o05em_class_by_id,
+  show_node_by_id,
+} from "../utils/utilities.js";
+
+const checkValidity = () => {
+  const ids_var = {
+    fn: "firstname",
+    ln: "lastname",
+    em: "email",
+    pw: "password",
+    fn_warn: "fn_div",
+    ln_warn: "ln_div",
+    em_warn: "em_div",
+    pw_warn: "pw_div",
+  };
+
+  let fnNode = document.getElementById(ids_var.fn);
+  let lnNode = document.getElementById(ids_var.ln);
+  let emNode = document.getElementById(ids_var.em);
+  let pwNode = document.getElementById(ids_var.pw);
+
+  // Check if it's not empty
+  // if empty =>
+  //  remove mb-1-25em on input,
+  //  add mb-05em on input,
+  //  show warning text
+
+  if (fnNode.value === "") {
+    remove_margin_bottom_1o25em_class_by_id(ids_var.fn);
+    add_margin_bottom_o05em_class_by_id(ids_var.fn);
+    show_node_by_id(ids_var.fn_warn);
+  }
+};
--- intro-component-with-signup-form-master/js/utils/utilities.js
+++ intro-component-with-signup-form-master/js/utils/utilities.js
@@ -1,7 +1,61 @@
-const show_warning_msg = () => {
-  let listNode = document.getElementsByClassName("align-warning-text");
-  for (let i = 0; i < listNode.length; i++) {
-    const wNode = listNode[i];
-    wNode.classList.remove("hidden");
-  }
+const utilities_css_class = {
+  hide_class: "hidden",
+  mb_1_25em_class: "mb-1-25em",
+  mb_05em_class: "mb-05em",
+};
+
+/**
+ * @param {string} id
+ * @return {undefined}
+ */
+export const hide_node_by_id = (id) =>
+  document.getElementById(id).classList.add(utilities_css_class.hide_class);
+
+/**
+ * @param {string} id
+ * @return {undefined}
+ */
+export const show_node_by_id = (id) => {
+  let node = document.getElementById(id);
+  // check if it contain hidden class
+  if (node.classList.contains(hide_class))
+    node.classList.remove(utilities_css_class.hide_class);
+};
+
+/**
+ * @param {string} id
+ * @return {undefined}
+ */
+export const remove_margin_bottom_1o25em_class_by_id = (id) => {
+  document
+    .getElementById(id)
+    .classList.remove(utilities_css_class.mb_1_25em_class);
+};
+
+/**
+ * @param {string} id
+ * @return {undefined}
+ */
+export const add_margin_bottom_1o25em_class_by_id = (id) => {
+  document
+    .getElementById(id)
+    .classList.add(utilities_css_class.mb_1_25em_class);
+};
+
+/**
+ * @param {string} id
+ * @return {undefined}
+ */
+export const remove_margin_bottom_o05em_class_by_id = (id) => {
+  document
+    .getElementById(id)
+    .classList.remove(utilities_css_class.mb_05em_class);
+};
+
+/**
+ * @param {string} id
+ * @return {undefined}
+ */
+export const add_margin_bottom_o05em_class_by_id = (id) => {
+  document.getElementById(id).classList.add(utilities_css_class.mb_05em_class);
 };
