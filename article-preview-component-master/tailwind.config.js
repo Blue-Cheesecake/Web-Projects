4bc39257d13b91f7e35311185d89190196d9b1a2
SINUT WATTANRPORN <s_w.piepie@outlook.com.au>
2022-03-16 00:33:04 +0700

    Update

--- article-preview-component-master/tailwind.config.js
+++ article-preview-component-master/tailwind.config.js
@@ -25,6 +25,10 @@ module.exports = {
       letterSpacing: {
         customWidest: "0.3em",
       },
+      width: {
+        30: "28rem",
+        40: "41rem",
+      },
     },
   },
   plugins: [],
