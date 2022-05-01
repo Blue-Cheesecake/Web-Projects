d478d3b8e14b36ec3cc4afb911c538862e5ae2c8
SINUT WATTANRPORN <s_w.piepie@outlook.com.au>
2022-05-01 23:36:50 +0700

    Starter Pack

---
+++ interactive-rating-component-main/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+// Css
+import "./styles/vendors/tailwind-output.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
---
+++ interactive-rating-component-main/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./src/*.{js, jsx}",
+    "./src/**/*.{js, jsx}",
+    "./public/*.{js, html}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
