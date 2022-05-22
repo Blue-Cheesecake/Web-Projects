235a13486ee4d7e7a8d093bc08083356809af39f
SINUT WATTANRPORN <s_w.piepie@outlook.com.au>
2022-05-22 21:43:00 +0700

    Starter Pack

---
+++ intro-section-with-dropdown-navigation-main/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style/dist/tailwind-output.vendors.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
---
+++ intro-section-with-dropdown-navigation-main/tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./src/**/*.{js, jsx}", "./public/*.{html, js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
