26487e6743e51e285f9dbe0ddecac13fb02b5429
SINUT WATTANRPORN <s_w.piepie@outlook.com.au>
2022-06-04 14:54:55 +0700

    feat: mobile navbar

--- intro-section-with-dropdown-nav/src/common/nav-bar/navbar.common.tsx
+++ intro-section-with-dropdown-nav/src/common/nav-bar/navbar.common.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { Menu } from "@headlessui/react";
+import React, { useState } from "react";
 
 // Components
 import { DropDown } from "../../components/dropdown/dropdown.component";
@@ -9,6 +8,8 @@ import icon_todo from "../../images/icon-todo.svg";
 import icon_calendar from "../../images/icon-calendar.svg";
 import icon_reminders from "../../images/icon-reminders.svg";
 import icon_planning from "../../images/icon-planning.svg";
+import icon_menu from "../../images/icon-menu.svg";
+import icon_close_menu from "../../images/icon-close-menu.svg";
 
 interface Item {
   icon: string | undefined;
@@ -41,9 +42,12 @@ const companyDropdown: DropDownItem = {
 };
 
 export const NavBar: React.FC = () => {
+  const [isDisplayingMobileNavbar, setIsDisplayingMobileNavbar] =
+    useState(false);
+
   return (
-    <nav className="bg-white">
-      <div className="p-8 flex justify-between items-center">
+    <nav className="bg-white w-full">
+      <div className="p-8 hidden md:flex justify-between items-center">
         <div className="flex justify-between items-center">
           <h2 className="text-4xl text-black mr-11 font-semibold">snap</h2>
           <div className="mr-11">
@@ -66,14 +70,34 @@ export const NavBar: React.FC = () => {
           </a>
         </div>
         <div>
-          <a href="#">Login</a>
-          <a href="#">Register</a>
+          <a href="#" className="mr-8">
+            Login
+          </a>
+          <a
+            href="#"
+            className="bg-almostWhite text-mediumGray hover:text-almostBlack py-3 px-3 md:py-4 md:px-7 rounded-2xl border-2 border-mediumGray hover:border-almostBlack text-sm md:text-base"
+          >
+            Register
+          </a>
         </div>
       </div>
 
       {/* Mobile menu */}
-      <div className="md:hidden">
-        <h2>Hello from mobile</h2>
+      <div className="md:hidden flex justify-between items-center p-5">
+        <h2 className="text-4xl text-black mr-11 font-semibold">snap</h2>
+        <div>
+          {/* menu button */}
+          <button
+            onClick={(e) => {
+              setIsDisplayingMobileNavbar(!isDisplayingMobileNavbar);
+            }}
+          >
+            <img
+              src={!isDisplayingMobileNavbar ? icon_menu : icon_close_menu}
+              alt="icon menu"
+            />
+          </button>
+        </div>
       </div>
     </nav>
   );
--- intro-section-with-dropdown-nav/src/pages/home.page.tsx
+++ intro-section-with-dropdown-nav/src/pages/home.page.tsx
@@ -19,12 +19,12 @@ export const Home: React.FC = () => {
         alt="image hero mobile"
         className="md:hidden w-full h-auto"
       />
-      <main className="py-8 px-24 w-full flex justify-center items-center">
+      <main className="px-6 py-12 md:py-8 md:px-24 w-full flex justify-center items-center">
         <section className="md:mr-40">
           <h1 className="text-center text-3xl font-bold  md:text-left md:text-8xl mb-8 md:mb-12">
-            Make <br /> remote work
+            Make <br className="hidden md:block" /> remote work
           </h1>
-          <p className="mb-8 md:mb-16 px-8 md:px-0 md:pr-36 text-center md:text-left text-xs md:text-xl text-mediumGray font-medium">
+          <p className="mb-8 md:mb-16 px-8 md:px-0 md:pr-36 text-center md:text-left text-xs scale-125 md:scale-100 md:text-xl text-mediumGray font-medium">
             Get your team in sync, no matter your location. Streamline
             processes, create team rituals, and watch productivity soar.
           </p>
