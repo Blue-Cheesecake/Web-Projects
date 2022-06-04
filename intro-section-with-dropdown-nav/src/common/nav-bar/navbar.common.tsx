421ffa1dcd7cbba7d6b6a8754116ac2d7300b8ca
SINUT WATTANRPORN <s_w.piepie@outlook.com.au>
2022-06-04 12:08:06 +0700

    feat: update navbar

--- intro-section-with-dropdown-nav/src/common/nav-bar/navbar.common.tsx
+++ intro-section-with-dropdown-nav/src/common/nav-bar/navbar.common.tsx
@@ -1,16 +1,80 @@
 import React from "react";
+import { Menu } from "@headlessui/react";
 
 // Components
 import { DropDown } from "../../components/dropdown/dropdown.component";
 
+// icons
+import icon_todo from "../../images/icon-todo.svg";
+import icon_calendar from "../../images/icon-calendar.svg";
+import icon_reminders from "../../images/icon-reminders.svg";
+import icon_planning from "../../images/icon-planning.svg";
+
+interface Item {
+  icon: string | undefined;
+  linkText: string;
+  path: string;
+}
+
+interface DropDownItem {
+  dropdownText: string;
+  items: Array<Item>;
+}
+
+const featuresDropdown: DropDownItem = {
+  dropdownText: "Features",
+  items: [
+    { icon: icon_todo, linkText: "Todo List", path: "#" },
+    { icon: icon_calendar, linkText: "Calendar", path: "#" },
+    { icon: icon_reminders, linkText: "Reminders", path: "#" },
+    { icon: icon_planning, linkText: "Planning", path: "#" },
+  ],
+};
+
+const companyDropdown: DropDownItem = {
+  dropdownText: "Company",
+  items: [
+    { icon: undefined, linkText: "History", path: "#" },
+    { icon: undefined, linkText: "Our Team", path: "#" },
+    { icon: undefined, linkText: "Blog", path: "#" },
+  ],
+};
+
 export const NavBar: React.FC = () => {
   return (
-    <nav className="bg-black">
-      <div>
-        <h2>snap</h2>
-        <a href="#">Careers</a>
+    <nav className="bg-white">
+      <div className="p-6 flex justify-between items-center">
+        <div className="flex justify-between items-center">
+          <h2 className="text-4xl text-black mr-6">snap</h2>
+          <div className="mr-6">
+            <DropDown
+              dropdownText={featuresDropdown.dropdownText}
+              items={featuresDropdown.items}
+            />
+          </div>
+          <div className="mr-6">
+            <DropDown
+              dropdownText={companyDropdown.dropdownText}
+              items={companyDropdown.items}
+            />
+          </div>
+          <a href="#" className="mr-6">
+            Careers
+          </a>
+          <a href="#" className="">
+            About
+          </a>
+        </div>
+        <div>
+          <a href="#">Login</a>
+          <a href="#">Register</a>
+        </div>
       </div>
-      <div></div>
+
+      {/* Mobile menu */}
+      <div className="md:hidden">
+        <h2>Hello from mobile</h2>
+      </div>
     </nav>
   );
 };
--- intro-section-with-dropdown-nav/src/components/dropdown/dropdown.component.tsx
+++ intro-section-with-dropdown-nav/src/components/dropdown/dropdown.component.tsx
@@ -1,5 +1,56 @@
 import React from "react";
+import { Menu, Transition } from "@headlessui/react";
 
-export const DropDown: React.FC = () => {
-  return <div>Dropdown</div>;
+interface Item {
+  icon: string | undefined;
+  linkText: string;
+  path: string;
+}
+
+interface Props {
+  dropdownText: string;
+  items: Array<Item>;
+}
+
+export const DropDown: React.FC<Props> = ({ dropdownText, items }: Props) => {
+  const mappedJSXItems: Array<JSX.Element> = items.map((item) => {
+    let icon: any = null;
+    if (item.icon) icon = <img src={item.icon} alt="icon" />;
+
+    return (
+      <Menu.Item>
+        {({ active }: { active: boolean }) => (
+          <div className="mb-3 w-full flex justify-start items-center">
+            {icon}
+            <a href={item.path} className="ml-4 text-sm">
+              {item.linkText}
+            </a>
+          </div>
+        )}
+      </Menu.Item>
+    );
+  });
+
+  return (
+    <Menu as="div" className="relative inline-block text-left">
+      <Menu.Button
+        as="div"
+        className="text-mediumGray hover:text-almostBlack cursor-pointer"
+      >
+        {dropdownText}
+      </Menu.Button>
+      <Transition
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items className="absolute top-3 right-0 pt-3 px-3 mt-2 w-32 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+          {mappedJSXItems}
+        </Menu.Items>
+      </Transition>
+    </Menu>
+  );
 };
