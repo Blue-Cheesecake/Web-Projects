60799d02ea19a83c6c7784ecfec3aedfdf9470c8
SINUT WATTANARPORN <s_w.piepie@outlook.com.au>
2022-12-24 17:58:37 +0700

    feat: starter

---
+++ Multi-Step-Form-Flutter/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(Main());
+
+class Main extends StatelessWidget {
+  const Main({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+
+    );
+  }
+}
