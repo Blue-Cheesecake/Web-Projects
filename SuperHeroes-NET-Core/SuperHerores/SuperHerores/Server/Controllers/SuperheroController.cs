2a6e4203c325ce82ca5d6542aeb8d5a67501a391
SINUT WATTANARPORN <s_w.piepie@outlook.com.au>
2023-01-10 23:51:42 +0700

    feat: post put del controller

--- SuperHeroes-NET-Core/SuperHerores/SuperHerores/Server/Controllers/SuperheroController.cs
+++ SuperHeroes-NET-Core/SuperHerores/SuperHerores/Server/Controllers/SuperheroController.cs
@@ -12,52 +12,88 @@ namespace SuperHerores.Server.Controllers
     [ApiController]
     public class SuperheroController : ControllerBase
     {
-        private static List<Comic> _Comics = new List<Comic> {
-            new Comic { Id=1, Name="Marvel"},
-            new Comic { Id=2, Name="DC"},
-        };
 
-        private static List<Superhero> _Heroes = new List<Superhero> {
-            new Superhero {
-                Id=1,
-                FirstName="Peter",
-                LastName="Parker",
-                HeroName="Spiderman",
-                Comic=_Comics[0],
-                ComicId=1,
-            },
-            new Superhero {
-                Id=2,
-                FirstName="Bruce",
-                LastName="Wayne",
-                HeroName="Batman",
-                Comic=_Comics[1],
-                ComicId=2,
-            },
-        };
+        private readonly DataContext _context;
+
+        public SuperheroController(DataContext context)
+        {
+            _context = context;
+        }
 
         [HttpGet]
         public async Task<IActionResult> GetSuperheroesList()
         {
-            return Ok(_Heroes);
+            return Ok(await _context.Superheroes
+                .Include(sh => sh.Comic)
+                .ToListAsync<Superhero>());
         }
 
         [HttpGet("comics")]
         public async Task<IActionResult> GetComics()
         {
-            return Ok(_Comics);
+            return Ok(await _context.Comics.ToListAsync<Comic>());
         }
 
         // or HTTPGet Route("{id}")
         [HttpGet("{id}")]
         public async Task<IActionResult> GetSingleSuperhero(int id)
         {
-            Superhero? result = _Heroes.Find(hero => hero.Id == id);
+            var superheroes = await _context.Superheroes
+                .Include(sh => sh.Comic)
+                .ToListAsync<Superhero>();
+            Superhero? result = superheroes.FirstOrDefault(e => e.Id == id);
             if (result == null)
             {
                 return NotFound("Sorry, no hero is found.");
             }
             return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateSuperHero(Superhero hero)
+        {
+            _context.Superheroes.Add(hero);
+            await _context.SaveChangesAsync();
+            return Ok(await GetSuperheroesAsync());
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteHero(int id)
+        {
+            var superhero = (await GetSuperheroesAsync())
+                .FirstOrDefault(e => e.Id == id);
+            if (superhero == null)
+            {
+                return NotFound("There's no hero to delete");
+            }
+
+            _context.Superheroes.Remove(superhero);
+            await _context.SaveChangesAsync();
+            return Ok(await GetSuperheroesAsync());
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateHero(Superhero newModel, int id)
+        {
+            var oldModel = (await GetSuperheroesAsync())
+                .FirstOrDefault(e => e.Id == id);
+            if (oldModel == null)
+            {
+                return NotFound("There's no hero to update");
+            }
+
+            oldModel.FirstName = newModel.FirstName;
+            oldModel.LastName = newModel.LastName;
+            oldModel.HeroName = newModel.HeroName;
+            oldModel.ComicId = oldModel.ComicId;
+
+            await _context.SaveChangesAsync();
+            return Ok(await GetSuperheroesAsync());
+        }
+
+        private async Task<List<Superhero>> GetSuperheroesAsync()
+        {
+            return await _context.Superheroes.ToListAsync();
         }
     }
 }
